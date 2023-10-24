5ec2d435e24dc540e8aaa7a29f2ea04889c08ab9
Jakob van Santen <jvansanten@gmail.com>
2023-10-24 11:47:41 +0200

    Validate task names

--- ampel/argo/models.py
+++ ampel/argo/models.py
@@ -1,3 +1,4 @@
+import re
 from pathlib import Path
 
 from pydantic import Field, validator
@@ -6,11 +7,13 @@ from ampel.model.job.JobModel import JobModel
 
 from .settings import settings
 
+NAME_REGEX = "[a-z0-9]([-a-z0-9]*[a-z0-9])?(\\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*"
+
 
 class ArgoJobModel(JobModel):
     name: str = Field(
         ...,
-        regex="[a-z0-9]([-a-z0-9]*[a-z0-9])?(\\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*",
+        regex=NAME_REGEX,
         description="Name of the job template. Must be a lowercase RFC 1123 subdomain name.",
     )
     image: str = Field(settings.ampel_image)
@@ -20,8 +23,17 @@ class ArgoJobModel(JobModel):
         if "/" in v:
             if not v.startswith(settings.container_registry):
                 raise ValueError(f"image not in approved registry")
-            else:
-                return f"{settings.container_registry}/{v}"
+            return v
+        else:
+            return f"{settings.container_registry}/{v}"
+
+    @validator("task")
+    def check_title(cls, v):
+        for item in v:
+            if not re.match(NAME_REGEX, item.title):
+                raise ValueError(
+                    f"invalid task title '{item.title}' (must be a lowercase RFC 1123 subdomain name)"
+                )
         return v
 
     class Config:
